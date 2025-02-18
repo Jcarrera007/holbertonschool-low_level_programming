65f0ef989208e2cc05d155f9135c8efb4d4c5894
Jcarrera007 <carrerasproductions@gmail.com>
2025-02-18 11:12:09 -0400

    fourth time

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,14 +11,17 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 9; num++)
+	for (num = 0; num < 10; num++)
 	{
 		putchar(num + '0');
-		putchar(',');
-		putchar(' ');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
-	putchar(9 + '0');
+	putchar('\n');
 
 	return (0);
 }
