9878fae7193c478961666d95d41edfddf030f2c4
Jcarrera007 <carrerasproductions@gmail.com>
2025-02-19 10:52:58 -0400

    third time

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,21 @@
-#include "main.h"  /* Ensure you have a header file with _putchar prototype */
+#include "main.h"
 
 /**
  * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
  */
 void print_alphabet_x10(void)
 {
-    char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-    int i = 0, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i = 0, j;
 
-    for (j = 0; j < 10; j++)
-    {
-        while (alphabet[i] != '\0')
-        {
-            _putchar(alphabet[i]);
-            i++;
-        }
-        i = 0;
-    }
+	for (j = 0; j < 10; j++)
+	{
+		while (alphabet[i] != '\0')
+		{
+			_putchar(alphabet[i]);
+			i++;
+		}
+		i = 0;
+	}
 }
 
