46132fd84150f7f001346485fec3503c502a1b4d
Jcarrera007 <carrerasproductions@gmail.com>
2025-02-24 10:29:38 -0400

    fixed the documentation.

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ *_isupper - checks if a character is uppercase.
+ *@c: the character to check
+
+ *return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 
 {
