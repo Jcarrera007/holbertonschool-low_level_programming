668874408033faac5a6bd10e0a54711c42a487ae
Jcarrera007 <carrerasproductions@gmail.com>
2025-02-24 12:36:06 -0400

    first one

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14,
+ * followed by a new line.
+ */
+void more_numbers(void)
+{
+	int i, j;
+	char numbers[] = "01234567891011121314\n";
+
+	for (i = 0; i < 10; i++)
+	{
+		j = 0;
+		while (numbers[j] != '\0')
+		{
+			_putchar(numbers[j]);
+			j++;
+		}
+	}
+}
+
