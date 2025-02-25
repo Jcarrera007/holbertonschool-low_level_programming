f9cf268bf560e1fe718b2696ce9f8ff82a55121c
Jcarrera007 <carrerasproductions@gmail.com>
2025-02-25 09:52:48 -0400

    fixed the problem

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,35 @@
 #include "main.h"
+#include <limits.h> /* Needed for INT_MAX and INT_MIN */
 
 /**
- * _atoi - Converts a string to an integer.
+ * _atoi - Converts a string to an integer safely.
  * @s: Pointer to the string.
  *
- * Return: The integer value of the string.
+ * Return: The integer value of the string, or 0 if no numbers found.
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int started = 0; /* Flag to check if a number has started */
+	int sign = 1, result = 0;
+	int digit;
 
-	while (*s) /* Iterate through the string */
+	while (*s)
 	{
-		if (*s == '-') /* Flip sign for each '-' encountered */
-			sign *= -1;
-		else if (*s == '+') /* Ignore '+' signs */
-			sign *= 1;
-		else if (*s >= '0' && *s <= '9') /* If it's a digit, process it */
+		if (*s == '-')
+			sign *= -1; /* Toggle sign for every '-' encountered */
+		else if (*s >= '0' && *s <= '9')
 		{
-			started = 1; /* Mark that we started processing numbers */
-			result = result * 10 + (*s - '0'); /* Build the integer */
+			digit = *s - '0';
+
+			/* Check for overflow before adding next digit */
+			if (result > (INT_MAX - digit) / 10)
+				return (sign == 1 ? INT_MAX : INT_MIN);
+
+			result = result * 10 + digit;
 		}
-		else if (started) /* Stop processing after numbers end */
+		else if (result > 0) /* Stop processing if numbers have started */
 			break;
 
-		s++; /* Move to the next character */
+		s++; /* Move to next character */
 	}
 
 	return (result * sign);
