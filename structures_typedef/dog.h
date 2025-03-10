8dd7342e97195a725c07b010d53b240dd7a9fb5b
Jcarrera007 <carrerasproductions@gmail.com>
2025-03-10 14:22:21 -0400

    first one

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a new structure for storing dog information.
+ * @name: Pointer to a string containing the dog's name.
+ * @age: The dog's age as a float.
+ * @owner: Pointer to a string containing the owner's name.
+ *
+ * Description: This structure holds information about a dog, including
+ * its name, age, and owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
