11261906d64966d9d4920ef157da0ea9bd667a23
Jimmy <carrerasproductions@gmail.com>
2025-03-17 13:40:47 -0400

    fixed the issue

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,37 +7,20 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i = 0;
-	char *str, *separator = "";
+    va_list args;
+    unsigned int i = 0;
+    char *str, *separator = "";
+    void (*print_func[])(va_list) = {
+        NULL, /* Placeholder for 'c' */
+        NULL, /* Placeholder for 'i' */
+        NULL, /* Placeholder for 'f' */
+        NULL  /* Placeholder for 's' */
+    };
 
-	va_start(args, format);  /* Initialize argument list */
+    va_start(args, format);
 
-	while (format && format[i])  /* First while loop */
-	{
-		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
-		{
-			printf("%s", separator);
-			separator = ", ";
-
-			if (format[i] == 'c')
-				printf("%c", va_arg(args, int));
-			if (format[i] == 'i')
-				printf("%d", va_arg(args, int));
-			if (format[i] == 'f')
-				printf("%f", va_arg(args, double));
-			if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
-				if (!str)
-					str = "(nil)";
-				printf("%s", str);
-			}
-		}
-		i++;
-	}
-
-	va_end(args);  /* Clean up argument list */
-	printf("\n");
-}
+    while (format && format[i])  /* First while loop */
+    {
+        /* Check valid format characters */
+        if (format[i] == 'c' || format[i] == 'i' || format[i] == '
 
