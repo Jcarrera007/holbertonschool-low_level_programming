e89197309b8e56da6d6f8ccaa306058adf4028f6
Jcarrera007 <carrerasproductions@gmail.com>
2025-04-04 01:29:00 -0400

    crossing fingers

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,11 +18,11 @@ void print_error(int code, const char *message, const char *arg)
 }
 
 /**
- * main - copies the contents of a file to another file
+ * main - copies the content of a file to another file
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0 on success, exits with appropriate code on failure
+ * Return: 0 on success, or exits with error code on failure
  */
 int main(int argc, char *argv[])
 {
@@ -37,15 +37,25 @@ int main(int argc, char *argv[])
 	if (fd_from == -1)
 		print_error(98, "Error: Can't read from file %s\n", argv[1]);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
 		print_error(99, "Error: Can't write to %s\n", argv[2]);
 	}
 
-	while ((r_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	while (1)
 	{
+		r_bytes = read(fd_from, buffer, BUFFER_SIZE);
+		if (r_bytes == -1)
+		{
+			close(fd_from);
+			close(fd_to);
+			print_error(98, "Error: Can't read from file %s\n", argv[1]);
+		}
+		if (r_bytes == 0)
+			break;
+
 		w_bytes = write(fd_to, buffer, r_bytes);
 		if (w_bytes == -1 || w_bytes != r_bytes)
 		{
@@ -55,25 +65,16 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if (r_bytes == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		print_error(98, "Error: Can't read from file %s\n", argv[1]);
-	}
-
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
 	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-
 	return (0);
 }
 
