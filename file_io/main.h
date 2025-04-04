55b8167eadb79fd6005e8ef00a0210d12b31232d
Jcarrera007 <carrerasproductions@gmail.com>
2025-04-04 02:51:22 -0400

    .............

--- file_io/main.h
+++ file_io/main.h
@@ -17,18 +17,13 @@
  * @message: The error message to print.
  * @file_name: The file name to include in the error message.
  */
-void error_exit(int exit_code, const char *message, const char *file_name)
-{
-    if (file_name)
-        dprintf(STDERR_FILENO, "%s %s\n", message, file_name);
-    else
-        dprintf(STDERR_FILENO, "%s\n", message);
-    exit(exit_code);
-}
+
 
 int _putchar(int c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(const char *file_from, const char *file_to);
+void error_exit(int code, const char *message, const char *arg);
 
 #endif /* MAIN_H */
