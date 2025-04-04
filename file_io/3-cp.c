7decb5e940261408f50b6e62535cc4782815fb3b
Jcarrera007 <carrerasproductions@gmail.com>
2025-04-04 01:03:44 -0400

    fixed the issue

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -27,9 +27,9 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		printf("98\n");
+		exit(98);}
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
