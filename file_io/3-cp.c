705027e4f53bbb58ad30172b668cc22c9bd7a4d9
Jcarrera007 <carrerasproductions@gmail.com>
2025-04-04 01:23:02 -0400

    6th time ive tried

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,34 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#define BUF_SIZE 1024
+#define BUFFER_SIZE 1024
 
 /**
- * print_error - prints an error message and exits with a code
+ * print_error - prints error and exits
  * @code: exit code
- * @format: format string
- * @arg: string to format into the message
+ * @message: format string
+ * @arg: argument for format
  */
-void print_error(int code, const char *format, const char *arg)
+void print_error(int code, const char *message, const char *arg)
 {
-	dprintf(STDERR_FILENO, format, arg);
+	dprintf(STDERR_FILENO, message, arg);
 	exit(code);
 }
 
 /**
- * main - copies content of a file to another file
+ * main - copies content from file_from to file_to
  * @argc: argument count
- * @argv: argument values
- *
- * Return: 0 on success, exits with specific error code otherwise
+ * @argv: argument vector
+ * Return: 0 on success, otherwise exits with a code
  */
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
 	ssize_t r_bytes, w_bytes;
-	char buffer[BUF_SIZE];
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 		print_error(97, "Usage: cp file_from file_to\n", "");
@@ -37,15 +36,22 @@ int main(int argc, char *argv[])
 	if (fd_from == -1)
 		print_error(98, "Error: Can't read from file %s\n", argv[1]);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
 		print_error(99, "Error: Can't write to %s\n", argv[2]);
 	}
 
-	while ((r_bytes = read(fd_from, buffer, BUF_SIZE)) > 0)
+	while ((r_bytes = read(fd_from, buffer, BUFFER_SIZE)) != 0)
 	{
+		if (r_bytes == -1)
+		{
+			close(fd_from);
+			close(fd_to);
+			print_error(98, "Error: Can't read from file %s\n", argv[1]);
+		}
+
 		w_bytes = write(fd_to, buffer, r_bytes);
 		if (w_bytes == -1 || w_bytes != r_bytes)
 		{
@@ -53,13 +59,6 @@ int main(int argc, char *argv[])
 			close(fd_to);
 			print_error(99, "Error: Can't write to %s\n", argv[2]);
 		}
-	}
-
-	if (r_bytes == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		print_error(98, "Error: Can't read from file %s\n", argv[1]);
 	}
 
 	if (close(fd_from) == -1)
