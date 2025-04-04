9a7a7159afa42bf67c10f36fb031ffe250b88973
Jcarrera007 <carrerasproductions@gmail.com>
2025-04-04 11:24:56 -0400

    ...

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,33 +40,36 @@ int main(int argc, char *argv[])
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-		error_exit(99, "Error: Can't write to", argv[2]);
+		error_exit(98, "Error: Can't write to", argv[2]);
 
-	r = read(fd_from, buffer, 1024);
-	while (r > 0)
+	while ((r = read(fd_from, buffer, 1024)) > 0)
 	{
-		w = write(fd_to, buffer, r);
-		if (w == -1 || w != r)
-			error_exit(99, "Error: Can't write to", argv[2]);
+		if (r == -1)
+		{
+			perror("Error in read"); /* It will print the specified error*/
+			error_exit(98, "Error: Can't read from file", argv[1]);
+		}
+		{
 
-		r = read(fd_from, buffer, 1024);
+			w = write(fd_to, buffer, r);
+			if (w == -1 || w != r)
+			{
+				perror("Error in write"); /* It will print the specified error*/
+				error_exit(99, "Error: Can't write to", argv[2]);
+			}
+		}
+		if (close(fd_from) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+			exit(100);
+		}
+
+		if (close(fd_to) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+			exit(100);
+		}
 	}
-
-	if (r == -1)
-		error_exit(98, "Error: Can't read from file", argv[1]);
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
 	return (0);
-}
 
+}
